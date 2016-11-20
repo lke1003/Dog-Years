392c0829db3f46e519465ad09181212d4f116dc5
LAO FILIPE JOSE <filipelao1003@gmail.com>
2016-11-20 15:39:12 +0800

    Initial Commit

---
+++ Dog Years/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Dog Years
+//
+//  Created by LAOFILIPE JOSE on 20/11/2016.
+//  Copyright © 2016 LAOFILIPE JOSE. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
