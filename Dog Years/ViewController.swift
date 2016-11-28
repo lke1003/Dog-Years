c8dc663b5ba0b28704651c566a0d7ab335304fb7
LAO FILIPE JOSE <filipelao1003@gmail.com>
2016-11-28 20:22:32 +0800

    Finish

--- Dog Years/ViewController.swift
+++ Dog Years/ViewController.swift
@@ -10,6 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var ageTextfield: UITextField!
+    @IBOutlet weak var ageLabel: UILabel!
+    
+    
+    @IBAction func submit(_ sender: Any) {
+        let dogAge = Int(ageTextfield.text!)! * 7
+        
+        ageLabel.text=String(dogAge)
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
